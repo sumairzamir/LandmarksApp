11c41217b5f96a4e69b1fb9951d1f03d1874b865
Sumair Zamir <sumair.zamir@gmail.com>
2020-10-03 17:54:31 +0100

    Initial Commit

---
+++ LandmarksApp/LandmarksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksAppApp.swift
+//  LandmarksApp
+//
+//  Created by Aiman Nabeel on 03/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
